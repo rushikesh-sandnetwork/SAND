2bded33405352199a91e181dca877e71a32a22c3
sonu4312 <sonu.kushwaha9872@gmail.com>
2025-02-03 17:20:13 +0530

    reducers commited

--- Sand/website/client/src/pages/users/admin/pages/AdminCreateForm/FormUtils/elements/FormFields/actions/fullNameActions.jsx
+++ Sand/website/client/src/pages/users/admin/pages/AdminCreateForm/FormUtils/elements/FormFields/actions/fullNameActions.jsx
@@ -1,12 +1,17 @@
 // fullNameActions.js
-import { SET_FULL_NAME_DATA } from './types';
+import { DELETE_FULL_NAME_DATA, SET_FULL_NAME_DATA } from "./types";
 
-export const setFullNameData = (id, fullName, componentName,options) => ({
+export const setFullNameData = (id, fullName, componentName, options) => ({
   type: SET_FULL_NAME_DATA,
   payload: {
     uniqueId: id,
     title: fullName,
     type: componentName,
-    options:options
+    options: options,
   },
+});
+
+export const deleteFullNameData = (id) => ({
+  type: DELETE_FULL_NAME_DATA,
+  payload: id,
 });
--- Sand/website/client/src/pages/users/admin/pages/AdminCreateForm/FormUtils/elements/FormFields/actions/types.jsx
+++ Sand/website/client/src/pages/users/admin/pages/AdminCreateForm/FormUtils/elements/FormFields/actions/types.jsx
@@ -1,3 +1,4 @@
 // actions/types.js
-export const SET_FULL_NAME_DATA = 'SET_FULL_NAME_DATA';
+export const SET_FULL_NAME_DATA = "SET_FULL_NAME_DATA";
 export const RESET_FULL_NAME_DATA = "RESET_FULL_NAME_DATA";
+export const DELETE_FULL_NAME_DATA = "DELETE_FULL_NAME_DATA";
