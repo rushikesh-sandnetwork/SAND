e0302b05bdbe223f58bbe7b7e489870c10a4d8e5
falguni.p02 <fparmar0205@gmail.com>
2025-03-21 13:56:14 +0530

    correction

--- Sand/website/client/src/vite.config.js
+++ Sand/website/client/src/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets load correctly
   server: {
-    port: 3000,
+    headers: {
+      'Content-Type': 'text/javascript',
+    },
   },
-});
+})
